98619bd6230d52dffa4b39ec0c674961f1863c5c
Adam Sadler <adambsadler@gmail.com>
2021-09-18 10:47:13 -0400

    Add project files.

---
+++ ProgramUI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UserInfoRepository;
+
+namespace PairedProgrammingGame
+{
+    class ProgramUI
+    {
+        public void Run()
+        {
+            string userInput = GetUserInfo();
+            if(userInput == "2")
+            {
+                return;
+            }
+            
+            Game();
+        }
+
+        private string GetUserInfo()
+        {
+            UserInfo userInfo = new UserInfo();
+            //Name
+            Console.WriteLine("Welcome to Just Another Day, a text based adventure game based on your life!\n" +
+                "To get started, let's get a little bit of information about yourself. What is your name?");
+            userInfo.Name = Console.ReadLine();
+            Console.Clear();
+
+            //City
+            Console.WriteLine($"Thank you, {userInfo.Name}!\n" +
+                $"Tell us the name of the city in which you live.");
+            userInfo.City = Console.ReadLine();
+            Console.Clear();
+
+            //Friend Name
+            Console.WriteLine($"I bet {userInfo.City} is a lovely place to live!\n" +
+                $"Tell us the name of a close friend");
+            userInfo.FriendName = Console.ReadLine();
+            Console.Clear();
+
+            // Age
+            Console.WriteLine($"{userInfo.FriendName} sounds lovely! \n" +
+                $"Tell us your age");
+            userInfo.Age = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine($"{userInfo.Age}? Nice!\n" +
+                $"Thank you for all that info!");
+            Console.Clear();
+
+            //Start of game option
+            Console.WriteLine("Ready to get started?\n" +
+                "1. Continue\n" +
+                "2. Exit");
+            string userOption = Console.ReadLine();
+            var validOption = false;
+            while (validOption == false)
+            {
+                switch (userOption)
+                {
+                    case "1":
+                        validOption = true;
+                        return userOption;
+                    case "2":
+                        Console.WriteLine("Have a good day!");
+                        Thread.Sleep(1500);
+                        return userOption;
+                    default:
+                        Console.WriteLine("That is not a valid choice. Please press 1 or 2.");
+                        userOption = Console.ReadLine();
+                        break;
+                }
+            }
+            return userOption;
+        }
+        private void Game()
+        {
+            var hasGun = false;
+
+            // var hasFood = false;
+            // var hasBeenHurt = false;
+            // var hasMedicine = false;
+
+            Console.Clear();
+
+            Console.WriteLine("Testing game!");
+
+            Console.ReadKey();
+        }
+
+    }
+}
