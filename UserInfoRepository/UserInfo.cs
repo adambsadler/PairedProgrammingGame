cbcb39038ff151a3f6978f0ee1ea2bd2b0dba833
Adam Sadler <adambsadler@gmail.com>
2021-09-18 10:48:55 -0400

    Moved Repo file over

---
+++ UserInfoRepository/UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInfoRepository
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string FriendName { get; set; }
+        public int Age { get; set; }
+
+        public UserInfo() { }
+
+        public UserInfo(string name, string city, string friendName, int age)
+        {
+            Name = name;
+            City = city;
+            FriendName = friendName;
+            Age = age;
+        }
+
+
+    }
+}
